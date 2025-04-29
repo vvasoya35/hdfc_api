78d0e4991a590403a285eb282a6458196c162600
vvasoya35 <vvasoya35@gmail.com>
2025-04-29 23:38:48 +0530

    first commit

--- transaction/settings.py
+++ transaction/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from decouple import config
 
-SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
