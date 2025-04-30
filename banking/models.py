a52640f116a29865d29e215cea785bd51322a6f8
vvasoya35 <vvasoya35@gmail.com>
2025-05-01 03:40:33 +0530

    first commit

--- banking/models.py
+++ banking/models.py
@@ -97,7 +97,6 @@ class TransactionConfig(models.Model):
     auth_url = models.URLField(default='')
     fund_transfer_url = models.URLField(default='')
     transaction_status_url = models.URLField(default='')
-    statement_url = models.URLField(default='',null=True, blank=True)
     source = models.CharField(max_length=10) 
     get_balance_url = models.URLField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
