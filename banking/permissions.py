5c33da4ab3e2702389c707a0d9ff202f6cfefdac
vvasoya35 <vvasoya35@gmail.com>
2025-05-01 01:27:42 +0530

    first commit

--- banking/permissions.py
+++ banking/permissions.py
@@ -13,10 +13,10 @@ class IsAuthorizedIP(BasePermission):
         allowed_ips = list(Users_ips.objects.values_list('ip_address', flat=True))
         # request_ip = request.META.get("HTTP_X_FORWARDED_FOR", "")
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        # if x_forwarded_for:
-        request_ip = x_forwarded_for.split(',')[0].strip()
-        # else:
-        #     request_ip = request.META.get('REMOTE_ADDR')
+        if x_forwarded_for:
+            request_ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            request_ip = request.META.get('REMOTE_ADDR')
 
         # print("allowed_ips ", allowed_ips)
         # print("request_ip ", request_ip)
@@ -33,10 +33,10 @@ def is_authorized_ip(request):
     allowed_ips = list(Users_ips.objects.values_list('ip_address', flat=True))
     # request_ip = request.META.get("HTTP_X_FORWARDED_FOR", "")
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    # if x_forwarded_for:
-    request_ip = x_forwarded_for.split(',')[0].strip()
-    # else:
-        # request_ip = request.META.get('REMOTE_ADDR')
+    if x_forwarded_for:
+        request_ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        request_ip = request.META.get('REMOTE_ADDR')
 
     # print("allowed_ips ", allowed_ips)
     # print("request_ip ", request_ip)
