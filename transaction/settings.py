749a84d1d924b99e166bef09963fb8e8294b8a49
vvasoya35 <vvasoya35@gmail.com>
2025-05-01 01:23:32 +0530

    first commit

--- transaction/settings.py
+++ transaction/settings.py
@@ -34,14 +34,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['connect.kachipukatextile.com','kachipukatextile.com','64.227.145.53','127.0.0.1','167.71.227.2']
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://connect.kachipukatextile.com",
-#     "https://www.kachipukatextile.com",
-#     "http://64.227.145.53:8000",
-#     "http://64.227.145.53",
-#     "http://167.71.227.2:8000",
-#     "http://167.71.227.2",
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "https://connect.kachipukatextile.com",
+    "https://www.kachipukatextile.com",
+    "http://64.227.145.53:8000",
+    "http://64.227.145.53",
+    "http://167.71.227.2:8000",
+    "http://167.71.227.2",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
